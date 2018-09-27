380e1ef44a56919421f31da8f12e43ef785c258c
ericzlmd <ericzlmd@gmail.com>
2018-09-27 16:44:01 -0400

    Changed JS syntax to ES6 standard

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var btn = document.querySelector("button");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+const bodyStyleBg = body.style.background;
+const btn = document.querySelector("button");
 
-function setGradient() {
+// BONUS! Add a random button to generate 
+// Mozilla Developer Network documentation 
+// 0 - 255
+const numberGen = () => Math.floor(Math.random() * 256);
+
+const setGradient = () => {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-
     css.textContent = body.style.background + ";";
-    // css.textContent 
 }
 
 // input => scans for input changes
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
-// you can do onclick="" on the html file, but separation of concerns!
 
-// BONUS! Add a random button to generate 
-function numberGen() {
-    // Mozilla Developer Network documentation 
-    // 0 - 255
-    return Math.floor(Math.random() * 256);
-}
+const generateRgb = () => "rgb(" + numberGen() + ", " + numberGen() + ", " + numberGen() + ")";
 
-function generateRgb() {
-    var a = numberGen();
-    var b = numberGen();
-    var c = numberGen();
-    var bgColor = "rgb(" + a + ", " + b + ", " + c + ")";
-    return bgColor;
-}
-
-function ranColors() {
-    var grad1 = generateRgb();
-    var grad2 = generateRgb();
-    body.style.background = "linear-gradient(to right, " + grad1 + ", " + grad2 + ")";
+const ranColors = () => {
+    body.style.background = "linear-gradient(to right, " + generateRgb() + ", " + generateRgb() + ")";
 
     css.textContent = body.style.background + ";";
 }
